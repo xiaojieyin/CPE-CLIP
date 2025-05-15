3c19353d5d370e4f1fb5cbb99053a4aa269ae78e
xiaojieyin <50098333+xiaojieyin@users.noreply.github.com>
2025-05-15 22:01:26 +0800

    Update evaluate.sh

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -1,6 +1,6 @@
 data_root=PATH/TO/DATASET
 DESC_PATH=./descriptions/image_datasets
-gpu=$1
+gpu=0,1
 testsets=oxford_flowers/dtd/oxford_pets/stanford_cars/ucf101/caltech101/food101/sun397/fgvc_aircraft/eurosat
 
 # number of descriptions per class
